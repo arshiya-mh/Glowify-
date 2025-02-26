816094377d857d29ec1c7afd3dbdf2ba409c7fd3
arshiya-mh <arshiyamh.pc@gmail.com>
2025-02-26 20:20:39 +0330

    initial version

---
+++ script.js
@@ -0,0 +1,17 @@
+// let brands = document.querySelector('#brnds');
+// let brandbox = document.querySelector('.brands');
+// brands.addEventListener('click', (e) => {
+//     brandbox.classList.toggle('active');
+// let brands = document.querySelector('#brnds');
+// let brandbox = document.querySelector('.brands');
+
+// brands.addEventListener('mouseenter', () => {
+//     brandbox.style.display = 'block';
+// });
+// brands.addEventListener('click', () => {
+//     brandbox.style.display = 'block';
+// });
+// brands.addEventListener('mouseleave', () => {
+//     brandbox.style.display = 'none';
+// });
+// 
